fd5087fb8fde9c5358cc6016cdd2cb1446a24bf9
williamvit93 <37162258+williamvit93@users.noreply.github.com>
2019-01-03 22:40:08 -0200

    ClimbingLeaderboard

--- HackerRankChallenges/Program.cs
+++ HackerRankChallenges/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -48,10 +49,20 @@ namespace HackerRankChallenges
             //var a = new List<int>() { 4, 97, 5, 97, 97, 4, 97, 4, 97, 97, 97, 97, 4, 4, 5, 5, 97, 5, 97, 99, 4, 97, 5, 97, 97, 97, 5, 5, 97, 4, 5, 97, 97, 5, 97, 4, 97, 5, 4, 4, 97, 5, 5, 5, 4, 97, 97, 4, 97, 5, 4, 4, 97, 97, 97, 5, 5, 97, 4, 97, 97, 5, 4, 97, 97, 4, 97, 97, 97, 5, 4, 4, 97, 4, 4, 97, 5, 97, 97, 97, 97, 4, 97, 5, 97, 5, 4, 97, 4, 5, 97, 97, 5, 97, 5, 97, 5, 97, 97, 97 };
 
             //pickingNumbers(a);
-            int[] scores = new int[] { 100, 100, 50, 40, 40, 20, 10 };
-            int[] alice = new int[] { 5, 25, 50, 120 };
+            //int[] scores = new int[] { 100, 100, 50, 40, 40, 20, 10 };
+            //int[] alice = new int[] { 5, 25, 50, 120 };
 
-            climbingLeaderboard(scores, alice);
+            //int[] scores = new int[] { 100, 90, 90, 80, 75, 60 };
+            //int[] alice = new int[] { 50, 65, 77, 90, 102 };
+
+            //int[] scores = new int[] { 997, 981, 957, 933, 930, 927, 926, 920, 916, 896, 887, 874, 863, 863, 858, 847, 815, 809, 803, 794, 789, 785, 783, 778, 764, 755, 751, 740, 737, 730, 691, 677, 652, 650, 587, 585, 583, 568, 546, 541, 540, 538, 531, 527, 506, 493, 457, 435, 430, 427, 422, 422, 414, 404, 400, 394, 387, 384, 374, 371, 369, 369, 368, 365, 363, 337, 336, 328, 325, 316, 314, 306, 282, 277, 230, 227, 212, 199, 179, 173, 171, 168, 136, 125, 124, 95, 92, 88, 85, 70, 68, 61, 60, 59, 44, 43, 28, 23, 13, 12 };
+            //int[] alice = new int[] { 12, 20, 30, 32, 35, 37, 63, 72, 83, 85, 96, 98, 98, 118, 122, 125, 129, 132, 140, 144, 150, 164, 184, 191, 194, 198, 200, 220, 228, 229, 229, 236, 238, 246, 259, 271, 276, 281, 283, 287, 300, 302, 306, 307, 312, 318, 321, 325, 341, 341, 341, 344, 349, 351, 354, 356, 366, 369, 370, 379, 380, 380, 396, 405, 408, 417, 423, 429, 433, 435, 438, 441, 442, 444, 445, 445, 452, 453, 465, 466, 467, 468, 469, 471, 475, 482, 489, 491, 492, 493, 498, 500, 501, 504, 506, 508, 523, 529, 530, 539, 543, 551, 552, 556, 568, 569, 571, 587, 591, 601, 602, 606, 607, 612, 614, 619, 620, 623, 625, 625, 627, 638, 645, 653, 661, 662, 669, 670, 676, 684, 689, 690, 709, 709, 710, 716, 724, 726, 730, 731, 733, 737, 744, 744, 747, 757, 764, 765, 765, 772, 773, 774, 777, 787, 794, 796, 797, 802, 805, 811, 814, 819, 819, 829, 830, 841, 842, 847, 857, 857, 859, 860, 866, 872, 879, 882, 895, 900, 900, 903, 905, 915, 918, 918, 922, 925, 927, 928, 929, 931, 934, 937, 955, 960, 966, 974, 982, 988, 996, 996 };
+
+
+            foreach (var item in climbingLeaderboard(scores, alice))
+            {
+                Console.WriteLine(item);
+            };
 
             Console.ReadLine();
         }
@@ -449,37 +460,73 @@ namespace HackerRankChallenges
 
         static int[] climbingLeaderboard(int[] scores, int[] alice)
         {
-            var leaderBoarder = new List<int>();
-            var ranking = scores.GroupBy(x => x).Select(l => l.Key).ToList();
+            //PERFORMÁTICO MÉDIA 15MS
 
-            foreach (var aliceScore in alice)
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            int h_rank = 1;
+            for (int i = scores.Length - 1; i > 0; i--)
             {
-                var temp = 0;
-
-                for (int i = 0; i < ranking.Count(); i++)
+                if (scores[i] < scores[i - 1])
                 {
-                    if (aliceScore < ranking[i])
+                    h_rank++;
+                }
+            }
+            h_rank++;
+            int[] ranks = new int[alice.Length];
+            int j = scores.Length - 1;
+            for (int i = 0; i < alice.Length; i++)
+            {
+                while (alice[i] >= scores[j])
+                {
+                    if (h_rank == 1)
                     {
-                        if (aliceScore <= ranking[ranking.Count() - 1])
+                        break;
+                    }
+                    if (j != 0)
+                    {
+                        if (scores[j] == scores[j - 1])
                         {
-                            temp = ranking.Count() + 1;
-                            leaderBoarder.Add(temp);
-
-                            break;
+                            j--;
                         }
-
+                        else
+                        {
+                            j--;
+                            h_rank--;
+                        }
                     }
                     else
                     {
-                        temp = i + 1;
-                        leaderBoarder.Add(temp);
-
-                        break;
+                        h_rank--;
                     }
-                }
-            }
 
-            return leaderBoarder.ToArray();
+                }
+                ranks[i] = h_rank;
+            }
+            //sw.Stop();
+            //Console.WriteLine("tempo gasto :  " + sw.ElapsedTicks);
+            return ranks;
+
+            //CÓDIGO LIMPO MÉDIA 20000MS
+
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+
+            //var leaderBoarder = new List<int>();
+            //List<int> groupingScores = scores.GroupBy(x => x).Select(l => l.Key).ToList();
+
+            //for (int i = 0; i < alice.Length; i++)
+            //{
+            //    int? ranking = scores.GroupBy(x => x).Select(l => l.Key).FirstOrDefault(x => x <= alice[i]);
+
+            //    var position = ranking == 0 ? groupingScores.Count() + 1 : groupingScores.FindIndex(x => x == ranking.Value) + 1;
+
+            //    leaderBoarder.Add(position);
+            //}
+            ////sw.Stop();
+            ////Console.WriteLine("tempo gasto :  " + sw.ElapsedTicks);
+            //return leaderBoarder.ToArray();
+
         }
     }
 }
